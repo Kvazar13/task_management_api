95311d4e5e6b5e421fad0a3a1a3313ce1b3775d8
kvazar13 <pavel_kozheykin@outlook.com>
2024-06-20 11:50:11 +0200

    feat(config.py): add support for SECRET_KEY, ALGORITHM, and ACCESS_TOKEN_EXPIRE_MINUTES environment variables to improve security and configurability

--- app/config.py
+++ app/config.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 TORTOISE_ORM = {
     "connections": {
